3dada7179c328416533f286273d745b8f34796e4
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2021-07-07 11:14:48 -0300

    New README

--- service/voteService.js
+++ service/voteService.js
@@ -1,12 +1,14 @@
 const { getParticipantsVotes, updateParticipantsVotes } = require('../models/voteModel');
 
-const notFound = 'notFound';
+const NOT_FOUND = 'not found';
+const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 
 async function addVote(id) {
   const participants = await getParticipantsVotes();
   const participant = participants.find((part) => part.id === id);
 
-  if (!participant) return { status: 404, message: { id, message: notFound } };
+  if (!participant) return { status: HTTP_NOT_FOUND_STATUS, message: { id, message: NOT_FOUND } };
 
   const voted = participants.map((part) => (
     (part.id === id)
@@ -15,7 +17,7 @@ async function addVote(id) {
 
   await updateParticipantsVotes(voted);
 
-  return { status: 200, message: { id, count: participant.count + 1 } };
+  return { status: HTTP_OK_STATUS, message: { id, count: participant.count + 1 } };
 }
 
 module.exports = {
